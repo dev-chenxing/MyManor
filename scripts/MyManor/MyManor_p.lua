57eaabf428ca727ee972e9722e415bbfa33c4aba
Zackhasacat <tobias@tribble.dev>
2023-07-13 22:32:00 -0500

    Added handing for older versions

--- scripts/MyManor/MyManor_p.lua
+++ scripts/MyManor/MyManor_p.lua
@@ -1,15 +1,22 @@
 local core = require("openmw.core")
+local types = require("openmw.types")
+local I = require("openmw.interfaces")
+if types.Player.quests == nil then
+    I.Settings.registerPage {
+        key = 'SettingsMyManor',
+        l10n = '',
+        name = 'MyManor',
+        description = 'This version of OpenMW has no lua quest support. Update to the latest 0.49 or development release/build.',
+    }
+    error("This version of OpenMW has no lua quest support. Update to the latest 0.49 or development release/build.")
+else
+end
 local function onQuestUpdate(id,index)
 if id == "jsmk_mm" then
     core.sendGlobalEvent("MManor_journalUpdated",index)
 end
 end
 return {
-	interfaceName  = "MyManor",
-	interface      = {
-		version = 1,
-
-	},
 	engineHandlers = {
 		onQuestUpdate = onQuestUpdate
 	}
